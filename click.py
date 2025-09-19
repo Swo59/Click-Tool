0209e6972ed8c33825b5f8e0c56d0d82a71d872a
Swo59 <62887566+Swo59@users.noreply.github.com>
2025-09-19 11:06:42 +0200

    Update click.py

--- click.py
+++ click.py
@@ -1,8 +1,13 @@
 import pyautogui
-from datetime import datetime
-now = datetime.time(datetime.now())
+from datetime import datetime , timedelta
+now = datetime.now()
 nowsec = now.hour*60*60 + now.minute*60+ now.second
 minutes = int(input("write the number of minutes for the loop"))
+end_time = nowsec + minutes*60
+
+end_time = now + timedelta(minutes=minutes)
+
+print("time at end = ", end_time ) #.strftime("%H:%M:%S"))
 
 thensec = nowsec + minutes*60
 isec = nowsec + 10
@@ -12,7 +17,9 @@ while thensec>nowsec :
     nowsec = now.hour*60*60 + now.minute*60+ now.second
     #print(nowsec)
     if nowsec == isec :
-        isec += 60
+        isec += 30
         print(now.strftime("%H:%M:%S"))
-        print("nowclick")
+        print("now click")
         pyautogui.click(10,10)
+
+
